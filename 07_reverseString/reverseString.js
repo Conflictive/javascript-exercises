a18b3509d5cb400235edd83617bfcafb9a9ee9e8
Conflictive <callumtolland@gmail.com>
2025-11-28 04:24:32 +0000

    Finish exercise 7
    Reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let newString = "";
+
+    for (let letter = string.length - 1; letter >= 0; letter--) {
+        newString += string[letter];
+    }
+
+    return newString;
 };
 
 // Do not edit below this line

60719a92909a913d809992a454b6ba5b7dde5b63
Conflictive <callumtolland@gmail.com>
2025-11-29 17:54:11 +0000

    Finish exercise 8
    Remove from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    
+    for (let i = 0; i < args.length; i++) {
+        arr = arr.filter((num) => num !== args[i])
+    }
+
+    return arr
 };
 
 // Do not edit below this line

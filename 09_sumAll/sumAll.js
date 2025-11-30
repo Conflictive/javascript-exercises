38aed8d5bfa19a64d4b51ed2e748c181fa259255
Conflictive <callumtolland@gmail.com>
2025-11-30 02:07:47 +0000

    Complete exercise 9
    Sum all

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {return "ERROR";}
+    if (firstNum < 0 || secondNum < 0) {return "ERROR";}
 
+    if (firstNum > secondNum) {
+        temp = firstNum;
+        firstNum = secondNum;
+        secondNum = temp;
+    }
+
+    let sum = 0;
+
+    for (let i = firstNum; i <= secondNum; i++) {
+        sum += i;
+    }
+
+    return sum
+    
 };
 
 // Do not edit below this line

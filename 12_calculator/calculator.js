765b3b6f81fe8eea841f47c7518707c27464c8f6
Conflictive <callumtolland@gmail.com>
2025-12-05 13:36:56 +0000

    Finish exercise 12
    calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((count, num) => count + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((count, num) => count * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num <= 1) {
+    return 1;
+  }
+
+  return num * factorial(num - 1)
 };
 
 // Do not edit below this line
@@ -31,3 +35,4 @@ module.exports = {
   power,
   factorial
 };
+

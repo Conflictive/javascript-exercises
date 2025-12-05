22511f83d21600cf430d861f8c2d18d949ff2684
Conflictive <callumtolland@gmail.com>
2025-12-05 14:58:02 +0000

    Finish exercise 14
    palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const grammar = [",", ".", "!", " ", "'"]
 
-};
+  wordArray = word.toLowerCase().split("")
+  console.log(wordArray)
+  
+  for (let i = 0; i < wordArray.length; i++) {
+    if (grammar.includes(wordArray[i])) {
+      wordArray.splice(i, 1);
+      i--
+    }
+  }
+  
+  word = wordArray.join("")
+  reversedWord = wordArray.reverse().join("");
+  console.log(word)
+  console.log(reversedWord)
+  
+  return word === reversedWord;
+}
 
 // Do not edit below this line
 module.exports = palindromes;

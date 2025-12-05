523399a09e9bc44ac935b0c78411309d3a455839
Conflictive <callumtolland@gmail.com>
2025-12-05 15:25:52 +0000

    Finish exercise 14
    Fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    } 
 
+    if (num == 0) {
+        return 0;
+    }
+    
+    let numOne = 0;
+    let numTwo = 1;
+
+    for (let i = 0; i < num - 1; i++) {
+        temp = numTwo;
+        numTwo = numOne + numTwo ;
+        numOne = temp;
+    }
+
+    return numTwo ;
 };
 
 // Do not edit below this line

7afc60d2313b2fefe7b15dbf3bdca7f3c23a5753
Conflictive <callumtolland@gmail.com>
2025-12-05 16:00:20 +0000

    Finish exercise 16
    Find the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let sortedArr = arr.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
 
+        const firstPerson = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        const secondPerson = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+
+        return secondPerson - firstPerson;
+    });
+
+    return sortedArr[0];
 };
 
 // Do not edit below this line
